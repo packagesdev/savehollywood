a60be240e2cfcf37c6558a814d671cd08d09c8d9
PackagesDev <stephane@stephanes-MacBook-Pro.local>
2014-11-09 09:50:21 +0100

    Start from where you left off feature
    Support in the engine
    Bump version

--- SaveHollywood/SHUserDefaults+Constants.m
+++ SaveHollywood/SHUserDefaults+Constants.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2012-2013, Stephane Sudre
+ Copyright (c) 2012-2014, Stephane Sudre
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -14,6 +14,7 @@
 #import "SHUserDefaults+Constants.h"
 
 NSString * const SHUserDefaultsAssetsRandomOrder=@"assets.randomOrder";
+NSString * const SHUserDefaultsAssetsRememberPosition=@"assets.rememberPosition";
 NSString * const SHUserDefaultsAssetsLibrary=@"assets.library";
 
 NSString * const SHUserDefaultsFrameScaling=@"frame.scaling";
